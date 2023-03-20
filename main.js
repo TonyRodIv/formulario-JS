448984eed7d4c2d15538c7d9f90c393acd9621c6
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-03-20 08:19:41 -0300

    Correção de bugs!!!
    - Correção do bug que enviava automaticamente o formulário mesmo estando errado.

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-let regex = /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,16}$/;
-function validate(){
-    let pass = document.getElementById('password').value
-    let passColor = document.getElementById('password')
-    if(regex.test(pass)){
-        alert('Seu login foi concluído com sucesso!! 😉👌🏽')
-    }else{
-        passColor.style.border = "red solid 3px"
-        alert('ERRO!! - Sua senha não atente os requisitos minímos')
+
+document.getElementById('form').addEventListener('submit',
+    function validate(event){
+        event.preventDefault();
+    
+        let regexP = /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,16}$/;
+        let regexE = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        
+        let email = document.getElementById('email').value
+        let pass = document.getElementById('password').value
+        let passColor = document.getElementById('password')
+        let emailColor = document.getElementById('email')
+        if(regexP.test(pass) && regexE.test(email)){
+            console.log('💥AMBOS ATENDEM OS REQUISITOS💥')
+            alert('Seu login foi concluído com sucesso!! 😉👌🏽')
+    
+        }
+    
+        if(regexP.test(pass)){
+            console.log('✅PASSWORD✅')
+            passColor.style.border = "green solid 3px"
+        }else{
+            console.log('⚠️PASSWORD⚠️')
+            passColor.style.border = "red solid 3px"
+        }
+    
+        if(regexE.test(email)){
+            console.log('✅EMAIL✅')
+            emailColor.style.border = "green solid 3px"
+        }else{
+            console.log('⚠️EMAIL⚠️')
+            emailColor.style.border = "red solid 3px"
+        }
     }
-}
+    )
